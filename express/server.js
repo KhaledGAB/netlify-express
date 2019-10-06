9d6770c9295f83aab65bf4866669489dbd275afb
Battot <khaled.battot212@gmail.com>
2019-10-06 07:33:23 +0200

    a7a

--- express/server.js
+++ express/server.js
@@ -6,13 +6,15 @@ const app = express();
 const bodyParser = require("body-parser");
 
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello from Express.js!</h1>");
-  res.end();
+app.get("/", (req, res) => {
+  console.log("a7a555");
+  res.status(200).send("done");
 });
-router.get("/another", (req, res) => res.json({ route: "ssasa5 55555" }));
-router.post("/", (req, res) => res.json({ postBody: req.body }));
+
+app.get("/another", (req, res) => {
+  console.log("a7a555");
+  res.status(200).send("done");
+});
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router); // path must route to lambda
